035c08047af46623d0bff3499dc4b6b6064a37a8
fliroajr <flavioliroa@gmail.com>
2024-07-17 00:17:48 -0300

    adjusting stuff

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.rds-mysql-labs.endpoint
-  sensitive   = true
+  sensitive   = false
 }
 
 output "rds_port" {
   description = "RDS instance port"
   value       = aws_db_instance.rds-mysql-labs.port
-  sensitive   = true
+  sensitive   = false
 }
 
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.rds-mysql-labs.username
-  sensitive   = true
+  sensitive   = false
 }
