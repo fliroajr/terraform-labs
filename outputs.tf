dd9dec03bc6740a7485fd2f5b099c675a72ab51f
fliroajr <flavioliroa@gmail.com>
2025-01-06 18:07:39 -0300

    feat/avoid-psql

--- outputs.tf
+++ outputs.tf
@@ -20,20 +20,20 @@ output "mysql_rds_username" {
 
 # postgres
 
-output "psql_rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.rds-psql-labs.endpoint
-  sensitive   = false
-}
+# output "psql_rds_hostname" {
+#   description = "RDS instance hostname"
+#   value       = aws_db_instance.rds-psql-labs.endpoint
+#   sensitive   = false
+# }
 
-output "psql_rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.rds-psql-labs.port
-  sensitive   = false
-}
+# output "psql_rds_port" {
+#   description = "RDS instance port"
+#   value       = aws_db_instance.rds-psql-labs.port
+#   sensitive   = false
+# }
 
-output "psql_rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.rds-psql-labs.username
-  sensitive   = true
-}
+# output "psql_rds_username" {
+#   description = "RDS instance root username"
+#   value       = aws_db_instance.rds-psql-labs.username
+#   sensitive   = true
+# }
